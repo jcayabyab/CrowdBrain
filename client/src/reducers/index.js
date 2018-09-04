22cc34106f1b376fdfbe4e175da38f5bd34068f2
Your Name <jjay.cayabyab@gmail.com>
2018-09-04 09:39:07 -0600

    added reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
+import projectsReducer from "./projectsReducer";
+import featuresReducer from "./featuresReducer";
+import commentsReducer from "./commentsReducer";
 
 export default combineReducers({
-  user: userReducer
+  user: userReducer,
+  projects: projectsReducer,
+  features: featuresReducer,
+  comments: commentsReducer
 });
