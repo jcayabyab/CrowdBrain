6277fed50a4f40b8eabc642ba9e6115e7775d583
Your Name <jjay.cayabyab@gmail.com>
2018-09-12 18:48:52 -0600

    finished

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,8 +1,25 @@
+import _ from "lodash";
+
 export default function(dateNum) {
   const timeElapsed = new Date().getTime() - dateNum;
-  const day = 1000 * 60 * 60 * 24;
 
-  if (timeElapsed > day) {
-    return Math.floor(timeElapsed / day)
-  }
+  const intervals = {
+    minute: 1000 * 60,
+    hour: 1000 * 3600,
+    day: 1000 * 86400,
+    week: 1000 * 86400 * 7,
+    month: 1000 * 86400 * 30
+  };
+
+  let output = null;
+
+  _.forEach(intervals, (value, key) => {
+    if (timeElapsed > value) {
+      const count = Math.floor(timeElapsed / value);
+
+      output = `${count} ${key}${count === 1 ? "" : "s"} ago`;
+    }
+  });
+
+  return output || `Just now`;
 }
