6cf0423d76840ba91e1ae37da175df0cf7f24b50
Your Name <jjay.cayabyab@gmail.com>
2018-09-25 12:16:24 -0600

    added coloring

--- client/src/components/Dashboard/ProjectListItem.js
+++ client/src/components/Dashboard/ProjectListItem.js
@@ -1,12 +1,51 @@
-import React, { Component } from 'react';
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled(Link)`
+  padding-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: stretch;
+  min-height: 50px;
+  position: relative;
+`;
+
+const CompletionLine = styled.div`
+  background-color: limegreen;
+  display: block;
+  height: 100%;
+  width: 3px;
+  position: absolute;
+`;
+
+const Title = styled.div`
+  margin-left: 10px;
+  display: flex;
+  align-items: center;
+`;
 
 class ProjectListItem extends Component {
   render() {
-    const {_id, title} = this.props.project;
+    const { _id, title } = this.props.project;
 
     return (
-      <Link to={`/p/${_id}`} className="list-group-item list-group-item-action">{title}</Link>
+      <Button
+        to={`/p/${_id}`}
+        className="list-group-item list-group-item-action"
+      >
+        <Wrapper>
+          <CompletionLine />
+          <Title>
+            <div>{title}</div>
+          </Title>
+        </Wrapper>
+      </Button>
     );
   }
 }
--- client/src/components/ProjectPage/FeatureListItem.js
+++ client/src/components/ProjectPage/FeatureListItem.js
@@ -1,16 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled(Link)`
+  padding-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: stretch;
+  min-height: 50px;
+  position: relative;
+`;
+
+const CompletionLine = styled.div`
+  background-color: limegreen;
+  display: block;
+  height: 100%;
+  width: 3px;
+  position: absolute;
+`;
+
+const Title = styled.div`
+  margin-left: 10px;
+  display: flex;
+  align-items: center;
+`;
 
 const FeatureListItem = ({ feature, projectId }) => {
   const { _id, title } = feature;
 
   return (
-    <Link
+    <Button
       to={`/p/${projectId}/${_id}`}
       className="list-group-item list-group-item-action"
     >
-      {title}
-    </Link>
+      <Wrapper>
+        <CompletionLine />
+        <Title>
+          <div>{title}</div>
+        </Title>
+      </Wrapper>
+    </Button>
   );
 };
 
